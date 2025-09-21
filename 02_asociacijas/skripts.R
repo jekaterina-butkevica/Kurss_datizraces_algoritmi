3b163ce9e65e8ab6858b205cb3275a8e49844239
Jekaterīna Butkeviča <jb22035@edu.lu.lv>
2025-09-21 23:33:17 +0300

    upd

--- 02_asociacijas/skripts.R
+++ 02_asociacijas/skripts.R
@@ -1,15 +1,16 @@
 # Ja nepieciešams, uzinstalē
 library(here)
 library(foreign)
+library(ggplot2)
 
-# Nolasām .data (vai .csv) failu
+# Nolasāggplot2# Nolasām .data (vai .csv) failu
 # header=TRUE, ja failā jau ir kolonnu nosaukumi
 # Teksta faila nolasīšana
 zoo <- read.csv("./02_asociacijas/Data/zoo.data", 
                 header = FALSE)
 
 
-write.arff(zoo, file = "mans_fails.arff")
+
 head(zoo)
 
 colnames(zoo) <- c("animal","hair","feathers","eggs","milk",
@@ -17,4 +18,10 @@ colnames(zoo) <- c("animal","hair","feathers","eggs","milk",
                    "backbone","breathes","venomous","fins",
                    "legs","tail","domestic","catsize","type")
 
+cols_to_factor <- c("hair","feathers","eggs","milk","airborne",
+                    "aquatic","predator","toothed","backbone",
+                    "breathes","venomous", "legs", "fins","tail","domestic","catsize","type")
+zoo[ , cols_to_factor] <- lapply(zoo[ , cols_to_factor], as.factor)
+
 write.arff(zoo, file = "./02_asociacijas/Data/zoo.arff")
+
